3821c44dd046f3789e48be1984ac1e8f1d684257
NadiaJ <student@PC11>
2013-08-16 11:04:39 +0530

    added new message

---
+++ 11031116/Movie2/Movie.java
@@ -0,0 +1,18 @@
+
+public class Movie {
+	 String movie_name;
+	  int price;
+	  int charges;
+	  
+	public Movie(String movie_name, int price) {
+		
+	}
+
+
+	public void charge(int days)
+	{
+		
+	}
+
+
+}
